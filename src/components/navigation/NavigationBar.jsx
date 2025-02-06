3bf9c2d60bd0468adf2980a8673eedcab35c934f
Bodya-Sav <bogdahnsavchuk@yandex.ru>
2025-02-06 14:14:18 +0300

    adding useEffect to navbar

--- src/components/navigation/NavigationBar.jsx
+++ src/components/navigation/NavigationBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Button } from "@telegram-apps/telegram-ui";
 import { ROUTES } from "../../navigation/routes";
