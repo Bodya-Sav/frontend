f92e814617b7f8035ef2dfe47b5d26bd1c3fef8b
Bodya-Sav <bogdahnsavchuk@yandex.ru>
2025-02-06 11:14:40 +0300

    fix to past fix

--- src/services/Service.js
+++ src/services/Service.js
@@ -1,4 +1,4 @@
-import API_URL from "./api";
+import { API_URL } from "./api";
 
 export const fetchWrapper = async (endpoint, method = "GET", body = null) => {
   try {
--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,1 @@
-export default API_URL = "https://pxmx-home.ddns.net:3001/api/mini_app";
+export const API_URL = "https://pxmx-home.ddns.net:3001/api/mini_app";
