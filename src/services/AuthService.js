e627f877a221d09a3915f58d6dddc4f3610ebda2
Bodya-Sav <bogdahnsavchuk@yandex.ru>
2025-02-07 16:42:08 +0300

    loggering authservices

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -2,5 +2,8 @@ import { fetchWrapper } from "./FetchWrapper";
 
 export const checkAuth = async (chat_id) => {
   const body = JSON.stringify({ chat_id });
-  return fetchWrapper("/check_auth", "POST", body);
+  const res = fetchWrapper("/check_auth", "POST", body);
+  console.log({ res });
+
+  return res;
 };
