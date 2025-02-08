2dee2ac3d6d369d6e7e4eb38c008c19e0e611150
Bodya-Sav <bogdahnsavchuk@yandex.ru>
2025-02-08 13:00:29 +0300

    fix useContext import

--- src/components/users/UserEditor.jsx
+++ src/components/users/UserEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Button } from "@telegram-apps/telegram-ui";
 import { updateUserInfo, getAllUsers } from "../../services/UserService";
 
