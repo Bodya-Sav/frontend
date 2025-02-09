ef67d34cf7e6de3b9d460b7d1e3c754d38c94d47
Bodya-Sav <bogdahnsavchuk@yandex.ru>
2025-02-09 11:45:58 +0300

    fix

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -2,6 +2,7 @@ import { fetchWrapper } from "./FetchWrapper";
 
 export const addCource = async ({ id, title }) => {
   console.log("Запрос добавления курса");
+  console.log(JSON.stringify({ id, title }));
 
   const body = JSON.stringify({ id, title });
   const res = await fetchWrapper("/add_course", "POST", body);
