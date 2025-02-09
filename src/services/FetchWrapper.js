13538d554cda36fab3a68d51d67c0a7bb78259f6
Bodya-Sav <bogdahnsavchuk@yandex.ru>
2025-02-09 13:52:53 +0300

    fix

--- src/services/FetchWrapper.js
+++ src/services/FetchWrapper.js
@@ -10,19 +10,16 @@ export const fetchWrapper = async (endpoint, method = "GET", body = null) => {
     };
 
     if (body) {
-      // Преобразуем тело запроса в JSON-строку
-      options.body = JSON.stringify(body);
+      options.body = body;
     }
 
     const response = await fetch(`${API_URL}${endpoint}`, options);
-    const data = await response.json();
 
     if (!response.ok) {
-      // Используем сообщение об ошибке, пришедшее с сервера
-      throw new Error(data.message || `Ошибка сети: ${response.status}`);
+      throw new Error(`Ошибка сети: ${response.status}`);
     }
 
-    return data;
+    return await response.json();
   } catch (error) {
     console.error(`Ошибка при запросе ${endpoint}:`, error);
     throw error;
