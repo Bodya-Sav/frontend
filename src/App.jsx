bdaa617c3ce1069f3d41bd537b7caceb9cd6c9e5
Bodya-Sav <bogdahnsavchuk@yandex.ru>
2025-02-09 13:58:43 +0300

    tring to log server answers

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         setId(data.result.id);
         setLoading(false);
       })
-      .catch((error) => console.error("Ошибка авторизации:", error));
+      .catch((error) => console.error("Ошибка авторизации:", error.message));
   }, []);
 
   if (loading) {
